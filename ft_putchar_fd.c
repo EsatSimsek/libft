81541f84223ff71d0ab6dc31cb130783e555fe57
Esat Şimşek <esatsimsek.6161@gmail.com>
2026-01-26 00:40:33 +0300

    maine dosya açma yazdırma eklendi

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: essimsek <essimsek@student.42istanbul.com. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 14:27:05 by essimsek          #+#    #+#             */
-/*   Updated: 2026/01/24 21:42:11 by essimsek         ###   ########.fr       */
+/*   Updated: 2026/01/26 00:39:57 by essimsek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
-/* 
-#include <stdio.h>
+
+/* #include <stdio.h>
+#include <fcntl.h>
 
 int	main(void)
 {
 	ft_putchar_fd('c', 1);
 	printf("\n");
-}
- */
+	int dosya_fd;
+	dosya_fd = open("dosya.txt", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	ft_putchar_fd('a', dosya_fd);
+} */
